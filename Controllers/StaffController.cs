1afdfa2d564bfec0a82d053ebe4153107d21fbe3
Boran ÖZDEMİR <117316618+boranozdemir@users.noreply.github.com>
2023-09-08 12:52:33 +0300

    Add files via upload

---
+++ Controllers/StaffController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StaffManagement.Controllers
+{
+    public class StaffController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
